c8c83d00427eafe5b8adae0b60514bd08a0d173c
IanManske <34794997+IanManske@users.noreply.github.com>
2020-01-28 18:14:03 -0500

    ItemWithSources update.

--- ItemWithSources.cs
+++ ItemWithSources.cs
@@ -2,31 +2,32 @@ using System.Collections.Generic;
 
 public abstract class ItemWithSources
 {
-		public int Price { get; }
-    
+	public int Price { get; set; }
+
+    private Sources SelectedSource;
     private readonly int CheapestPrice;
-    private readonly Sources SelectedSource, CheapestSource;
+    private readonly Sources CheapestSource;
     private readonly Dictionary<Sources, int> PriceFrom;
 		
-		public ItemWithSources (Dictionary<Sources, int> priceFrom)
-		{
-				PriceFrom = priceFrom;
-        CheapestPrice = Int64.MaxValue;
+	public ItemWithSources (Dictionary<Sources, int> priceFrom)
+	{
+		PriceFrom = priceFrom;
+        CheapestPrice = int.MaxValue;
         CheapestSource = 0;
         foreach (KeyValuePair<Sources, int> pair in PriceFrom)
         {
             if (pair.Value < CheapestPrice)
             {
                 CheapestPrice = pair.Value;
-                CHeapestSource = pair.Key;
+                CheapestSource = pair.Key;
             }
         }
         PriceFrom.Remove(CheapestSource);
         Price = CheapestPrice;
         SelectedSource = CheapestSource;
-		}
+	}
 		
-		public void TrySetSource(Sources source)
+	public void TrySetSource(Sources source)
     {
         if (SelectedSource != source && PriceFrom.ContainsKey(source))
         {
