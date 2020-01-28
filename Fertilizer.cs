0b5a9d30ccc12046bbbbb6079625940f1fade066
Ivordir <34794997+Ivordir@users.noreply.github.com>
2020-01-28 10:58:20 -0500

    Update Fertilizer.cs

--- Fertilizer.cs
+++ Fertilizer.cs
@@ -1,18 +1,20 @@
-﻿namespace StardewValleyBestCropPlanFinder.Client
+using System.Collections.Generic;
+
+namespace StardewValleyBestCropPlanFinder.Client
 {
     public class Fertilizer
     {
         public string Name { get; }
         public int Quality { get; }
         public float Speed { get; }
-        public int Price { get; }
+        public Dictionary<Sources, int> Sources { get; }
 
-        public Fertilizer(string name, int quality, float speedMultiplier, int price)
+        public Fertilizer(string name, int quality, float speed, Dictionary<Sources, int> sources)
         {
             Name = name;
             Quality = quality;
-            Speed = speedMultiplier;
-            Price = price;
+            Speed = speed;
+            Sources = sources;
         }
     }
 
