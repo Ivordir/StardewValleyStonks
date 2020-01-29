60ca1029d22187965019b20540530a5bcedfccc6
Ivordir <34794997+Ivordir@users.noreply.github.com>
2020-01-29 07:09:55 -0500

    Update ItemWithSources.cs

--- ItemWithSources.cs
+++ ItemWithSources.cs
@@ -4,41 +4,41 @@ public abstract class ItemWithSources
 {
 	public int Price { get; set; }
 
-    private Sources SelectedSource;
-    private readonly int CheapestPrice;
-    private readonly Sources CheapestSource;
-    private readonly Dictionary<Sources, int> PriceFrom;
-		
+	private Sources SelectedSource;
+	private readonly int CheapestPrice;
+	private readonly Sources CheapestSource;
+	private readonly Dictionary<Sources, int> PriceFrom;
+
 	public ItemWithSources (Dictionary<Sources, int> priceFrom)
 	{
 		PriceFrom = priceFrom;
-        CheapestPrice = int.MaxValue;
-        CheapestSource = 0;
-        foreach (KeyValuePair<Sources, int> pair in PriceFrom)
-        {
-            if (pair.Value < CheapestPrice)
-            {
-                CheapestPrice = pair.Value;
-                CheapestSource = pair.Key;
-            }
-        }
-        PriceFrom.Remove(CheapestSource);
-        Price = CheapestPrice;
-        SelectedSource = CheapestSource;
+		CheapestPrice = int.MaxValue;
+		CheapestSource = 0;
+		foreach (KeyValuePair<Sources, int> pair in PriceFrom)
+		{
+			if (pair.Value < CheapestPrice)
+			{
+				CheapestPrice = pair.Value;
+				CheapestSource = pair.Key;
+			}
+		}
+		PriceFrom.Remove(CheapestSource);
+		Price = CheapestPrice;
+		SelectedSource = CheapestSource;
 	}
-		
+
 	public void TrySetSource(Sources source)
-    {
-        if (SelectedSource != source && PriceFrom.ContainsKey(source))
-        {
-            Price = PriceFrom[source];
-            SelectedSource = source;
-        }
-    }
-    
-    public void SetCheapestSource()
-    {
-        Price = CheapestPrice;
-        SelectedSource = CheapestSource;
-    }
+	{
+		if (SelectedSource != source && PriceFrom.ContainsKey(source))
+		{
+			Price = PriceFrom[source];
+			SelectedSource = source;
+		}
+	}
+
+	public void SetCheapestSource()
+	{
+		Price = CheapestPrice;
+		SelectedSource = CheapestSource;
+	}
 }
