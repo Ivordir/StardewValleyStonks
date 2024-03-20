f3d547352152de9d521adf26e71312238d6bdefb
Ivordir <Ivordir@pm.me>
2024-03-20 11:27:09 -0400

    Update extractor for 1.6

--- src/Extractor/Program.fs
+++ src/Extractor/Program.fs
@@ -110,7 +110,8 @@ module Decode =
 
 [<AutoOpen>]
 module Constants =
-  let [<Literal>] forageSpriteSheetRow = 23u
+  let [<Literal>] unknownIdStart = 4000u
+  let [<Literal>] forageSpriteIndex = 23u
   let [<Literal>] cropImageWidth = 16
   let [<Literal>] cropImageHeight = 32
   let [<Literal>] itemImageWidth = 16
@@ -118,150 +119,173 @@ module Constants =
 
   let [<Literal>] configPath = "config.json"
 
-  let [<Literal>] cropDataPath = "Content/Data/Crops"
-  let [<Literal>] itemDataPath = "Content/Data/ObjectInformation"
-  let [<Literal>] cropSpriteSheetPath = "Content/TileSheets/crops"
-  let [<Literal>] itemSpriteSheetPath = "Content/Maps/springobjects"
+  let [<Literal>] cropDataPath = "Data\\Crops"
+  let [<Literal>] itemDataPath = "Data\\Objects"
+  let [<Literal>] cropSpriteSheetPath = "TileSheets\\crops"
+  let [<Literal>] itemSpriteSheetPath = "Maps\\springobjects"
 
 
-let parseCategory (itemId: ItemId) = function
-  | "Seeds -74" -> Seeds
-  | "Basic -75" -> Vegetable
-  | "Basic -79" -> Fruit
-  | "Basic -80" -> Flower
-  | "Basic -81" -> Forage
-  | "Basic -26" -> ArtisanGood
-  | "Basic -17" // sweet gem berry
-  | "Crafting" // (coffee)
-  | "Basic" // sugar, flour, etc
-  | "Basic -16" // building resources (fiber)
-    -> Other
-  | str -> failwith $"Unexpected item category for item {itemId}: {str}"
+let tryParseNat (str: string) =
+  match System.UInt32.TryParse str with
+  | true, n -> Some n
+  | false, _ -> None
 
-let parseItem overrides itemData itemId =
-  match itemData |> Table.tryFind itemId with
-  | None -> failwith $"Could not find data for item with id: {itemId}"
-  | Some (str: string) ->
-    let split = str.Split '/'
-    if split.Length < 5 then failwith $"Unexpected item data format for item {itemId}: {str}"
+let processCategory (itemId: string) = function
+  | -74 -> Seeds
+  | -75 -> Vegetable
+  | -79 -> Fruit
+  | -80 -> Flower
+  | -81 -> Forage
+  | -26 -> ArtisanGood
+  | -17 (* sweet gem berry *) | 0 (* coffee, sugar, flour, etc *) | -16 (* fiber *) -> Other
+  | str -> failwith $"Item {itemId} has an unsupported category: {str}"
+
+let processItem overrides itemData strId itemId =
+  match itemData |> Table.tryFind strId with
+  | None -> failwith $"Could not find data for item with id: {strId}"
+  | Some (item: StardewValley.GameData.Objects.ObjectData) ->
     let overrides = overrides |> Table.tryFind itemId |> Option.defaultValue ItemOverride.none
+    let category = overrides.Category |> Option.defaultWith (fun () -> processCategory strId item.Category)
+    let sellPrice = overrides.SellPrice |> Option.defaultWith (fun () ->
+      if item.Price < 0
+      then failwith $"Item {strId} has a negative price: {item.Price}"
+      else nat item.Price)
+
+    let spriteIndex =
+      if item.SpriteIndex < 0
+      then failwith $"Item {strId} has a negative sprite index: {item.SpriteIndex}"
+      else nat item.SpriteIndex
+
+    (spriteIndex, item.Texture),
     {
       Id = itemId
-      Name = split[4]
-      SellPrice = overrides.SellPrice |> Option.defaultWith (fun () -> nat split[1])
-      Category = overrides.Category |> Option.defaultWith (fun () -> parseCategory itemId split[3])
+      Name = item.Name
+      SellPrice = sellPrice
+      Category = category
     }
 
-let parseCropAmount seed overrides scythe (cropAmount: string) =
-  match cropAmount.Split ' ' with
-  | [| "false" |] ->
-    (CropAmount.singleAmount, overrides) ||> Option.fold (fun amount overrides ->
-      { amount with
-          CanDouble = overrides.CanDouble |> Option.defaultValue amount.CanDouble
-          FarmingQualities = overrides.FarmingDistribution |> Option.defaultValue amount.FarmingQualities
-      })
+let processCrop farmCropOverrides forageCropData assignId strId seed (crop: StardewValley.GameData.Crops.CropData) =
+  // Some currently ignored fields that might be useful in the future:
+  // crop.IsRaised
+  // crop.NeedsWatering
+  // crop.PlantableLocationRules
 
-  | [| "true"; minHarvest; maxHarvest; increasePerLevel; extraChance |] ->
-    let overrides = overrides |> Option.defaultValue CropAmountOverride.none
+  let seasons =
+    crop.Seasons
+    |> Seq.map (function
+      | StardewValley.Season.Spring -> Season.Spring
+      | StardewValley.Season.Summer -> Season.Summer
+      | StardewValley.Season.Fall -> Season.Fall
+      | StardewValley.Season.Winter -> Season.Winter
+      | season -> failwith $"Crop {strId} has an unknown season: {season}")
+    |> Seasons.ofSeq
 
-    let minHarvest = uint minHarvest
-    if minHarvest < CropAmount.minYield then
-      failwith $"The minimum harvest for crop {seed} was below {CropAmount.minYield}."
+  let growthStages =
+    crop.DaysInPhase
+    |> Seq.map (fun stage ->
+      if stage <= 0
+      then failwith $"Crop {strId} has a negative or zero growth stage: {stage}"
+      else nat stage)
+    |> Array.ofSeq
 
-    let maxHarvest = uint maxHarvest
-    if minHarvest > maxHarvest then
-      failwith $"The min harvest ({minHarvest}) was greater than the max harvest ({maxHarvest}) for crop {seed}."
+  if growthStages.Length = 0 then
+    failwith $"Crop {strId} has no growth stages"
 
-    let extraChance = float extraChance
-    if extraChance < CropAmount.minExtraCropChance || CropAmount.maxExtraCropChance < extraChance then
-      failwith $"The extra crop chance for crop {seed} was not in the range [{CropAmount.minExtraCropChance}, {CropAmount.maxExtraCropChance}]."
+  let regrowTime =
+    match crop.RegrowDays with
+    | -1 -> None
+    | time when time <= 0 -> failwith $"Crop {strId} had a negative or zero regrow time: {time}"
+    | time -> Some (nat time)
 
-    {
+  let minHarvest =
+    if crop.HarvestMinStack < int CropAmount.minYield
+    then failwith $"Crop {strId} has a minimum harvest below {CropAmount.minYield}: {crop.HarvestMinStack}"
+    else nat crop.HarvestMinStack
+
+  let maxHarvest =
+    if crop.HarvestMaxStack < int CropAmount.minYield
+    then failwith $"Crop {strId} has a maximum harvest below {CropAmount.minYield}: {crop.HarvestMaxStack}"
+    else nat crop.HarvestMaxStack |> max minHarvest
+
+  let increasePerLevel =
+    if crop.HarvestMaxIncreasePerFarmingLevel < 0.0f
+    then failwith $"Crop {strId} has a negative maximum harvest increase per level: {crop.HarvestMaxIncreasePerFarmingLevel}"
+    else nat (1.0f / crop.HarvestMaxIncreasePerFarmingLevel)
+
+  let extraChance =
+    if crop.ExtraHarvestChance < CropAmount.minExtraCropChance || CropAmount.maxExtraCropChance < crop.ExtraHarvestChance
+    then failwith $"Crop {strId} has an extra crop chance not in the range [{CropAmount.minExtraCropChance}, {CropAmount.maxExtraCropChance}]: {crop.ExtraHarvestChance}"
+    else crop.ExtraHarvestChance
+
+  let scythe =
+    match crop.HarvestMethod with
+    | StardewValley.GameData.Crops.HarvestMethod.Grab -> false
+    | StardewValley.GameData.Crops.HarvestMethod.Scythe -> true
+    | method -> failwith $"Crop {strId} has an unknown harvest method: {method}"
+
+  let paddy = crop.IsPaddyCrop
+
+  let spriteIndex =
+    if crop.SpriteIndex < 0
+    then failwith $"Crop {strId} has a negative sprite index: {crop.SpriteIndex}"
+    else nat crop.SpriteIndex
+
+  spriteIndex,
+  if spriteIndex = forageSpriteIndex then
+    let season =
+      match seasons with
+      | Seasons.Spring -> Season.Spring
+      | Seasons.Summer -> Season.Summer
+      | Seasons.Fall -> Season.Fall
+      | Seasons.Winter -> Season.Winter
+      | seasons -> failwith $"Forage crop {strId} does not grow in a single season: {seasons}"
+
+    if regrowTime.IsSome then failwith $"Forage crop {strId} regrows"
+    if paddy then failwith $"Forage crop {strId} is a paddy crop"
+    if scythe then failwith $"Forage crop {strId} has the scythe harvest method"
+
+    let data =
+      match forageCropData |> Table.tryFind season with
+      | Some data -> data
+      | None -> failwith $"No data was provided for {Season.name season} Forage in the config"
+
+    if string data.Foragables[0] <> crop.HarvestItemId then
+      failwith $"The main item provided for {Season.name season} Forage in the config does not match {crop.HarvestItemId}"
+
+    ForageCrop {
+      Season = season
+      Stages = growthStages
+      Seed = seed
+      Foragables = data.Foragables |> Array.map (string >> assignId)
+      SeedRecipeUnlockLevel = data.SeedRecipeUnlockLevel
+    }
+
+  else
+    let overrides = farmCropOverrides |> Table.tryFind seed |> Option.defaultValue FarmCropOverride.none
+    let amountOverride = overrides.Amount |> Option.defaultValue CropAmountOverride.none
+    let cropAmount = {
       MinCropYield = minHarvest
       MaxCropYield = maxHarvest
-      FarmLevelsPerYieldIncrease = uint increasePerLevel
+      FarmLevelsPerYieldIncrease = nat increasePerLevel
       ExtraCropChance = extraChance
-      CanDouble = overrides.CanDouble |> Option.defaultValue (not scythe)
-      FarmingQualities = overrides.FarmingDistribution |> Option.defaultValue true
+      CanDouble = amountOverride.CanDouble |> Option.defaultValue (not scythe)
+      FarmingQualities = amountOverride.FarmingDistribution |> Option.defaultValue true
     }
 
-  | _ -> failwith $"Unexpected crop amount format for crop {seed}: {cropAmount}"
+    let item = assignId crop.HarvestItemId
+    let extraItem = overrides.ExtraItem |> Option.map (fun (itemId, amount) -> itemId |> string |> assignId, amount)
 
-
-let parseCrop farmCropOverrides forageCropData seed (data: string) =
-  match data.Split '/' with
-  | [| growthStages; seasons; spriteSheetRow; itemId; regrowTime; scythe; cropAmount; _; _ |] ->
-    let spriteSheetRow = uint spriteSheetRow
-    let itemId = nat itemId * 1u<_>
-
-    let growthStages =
-      let split = growthStages.Split ' '
-      if Array.isEmpty split then failwith $"The crop {seed} had no growth stages." else
-      let stages = split |> Array.map uint
-      if stages |> Array.contains 0u then failwith $"The crop {seed} had a growth stage of 0."
-      stages
-
-    let seasons = (Seasons.None, seasons.Split ' ') ||> Array.fold (fun seasons season ->
-      Seasons.Parse (season, true) ||| seasons)
-
-    let regrowTime =
-      match regrowTime with
-      | "0" -> failwith $"The crop {seed} had a regrow time of 0."
-      | "-1" -> None
-      | time -> Some (uint time)
-
-    spriteSheetRow,
-    if spriteSheetRow = forageSpriteSheetRow then
-      let season =
-        match seasons with
-        | Seasons.Spring -> Season.Spring
-        | Seasons.Summer -> Season.Summer
-        | Seasons.Fall -> Season.Fall
-        | Seasons.Winter -> Season.Winter
-        | _ -> failwith $"Forage crop {seed} does not grow in a single season."
-
-      if regrowTime.IsSome then failwith $"Forage crop {seed} regrows."
-
-      let data =
-        match forageCropData |> Table.tryFind season with
-        | Some data -> data
-        | None -> failwith $"No data was provided for {Season.name season} Forage in the config."
-
-      if data.Foragables[0] <> itemId then
-        failwith "The main item provided for {Season.name season} Forage does not match {itemId}."
-
-      ForageCrop {
-        Season = season
-        Stages = growthStages
-        Seed = seed
-        Foragables = data.Foragables
-        SeedRecipeUnlockLevel = data.SeedRecipeUnlockLevel
-      }
-
-    else
-      let overrides = farmCropOverrides |> Table.tryFind seed |> Option.defaultValue FarmCropOverride.none
-
-      let scythe =
-        match scythe with
-        | "0" -> false
-        | "1" -> true
-        | str -> failwith $"Unexpected scythe value for crop {seed}: {str}"
-
-      let cropAmount = parseCropAmount seed overrides.Amount scythe cropAmount
-
-      FarmCrop {
-        Seasons = overrides.Seasons |> Option.defaultValue seasons
-        Stages = growthStages
-        RegrowTime = regrowTime
-        Paddy = overrides.Paddy |> Option.defaultValue false
-        Giant = overrides.Giant |> Option.defaultValue false
-        Seed = seed
-        Amount = cropAmount
-        Item = itemId
-        ExtraItem = overrides.ExtraItem
-      }
-
-  | _ -> failwith $"Unexpected crop data format for crop {seed}: {data}"
+    FarmCrop {
+      Seasons = overrides.Seasons |> Option.defaultValue seasons
+      Stages = growthStages
+      RegrowTime = regrowTime
+      Paddy = overrides.Paddy |> Option.defaultValue paddy
+      Giant = overrides.Giant |> Option.defaultValue false
+      Seed = seed
+      Amount = cropAmount
+      Item = item
+      ExtraItem = extraItem
+    }
 
 let saveSubTexture name graphics x y width height (texture: Texture2D) =
   let sub = Array.zeroCreate (width * height)
@@ -271,12 +295,12 @@ let saveSubTexture name graphics x y width height (texture: Texture2D) =
   use file = File.OpenWrite (name + ".png")
   subTexture.SaveAsPng (file, width, height)
 
-let saveStageImages graphics outputPath cropSpriteSheet spriteSheetRow crop =
+let saveStageImages graphics outputPath cropSpriteSheet spriteIndex crop =
   let path = Path.Combine (outputPath, string (Crop.seed crop))
   Directory.CreateDirectory path |> ignore
 
-  let x = if spriteSheetRow % 2u = 0u then 0 else 128
-  let y = (int spriteSheetRow / 2) * cropImageHeight
+  let x = if spriteIndex % 2u = 0u then 0 else 128
+  let y = (int spriteIndex / 2) * cropImageHeight
 
   let save name i =
     saveSubTexture
@@ -296,16 +320,16 @@ let saveStageImages graphics outputPath cropSpriteSheet spriteSheetRow crop =
 
   if Crop.regrows crop then save "Regrow" (stages.Length + 2)
 
-let saveItemImage graphics outputPath (itemSpriteSheet: Texture2D) item =
-  let id = int item.Id
+let saveItemImage graphics outputPath spriteSheets (spriteIndex, spriteSheet) item =
+  let spriteSheet: Texture2D = spriteSheets |> Table.find (if spriteSheet = null then itemSpriteSheetPath else spriteSheet)
   saveSubTexture
-    (Path.Combine (outputPath, string id))
+    (Path.Combine (outputPath, string item.Id))
     graphics
-    (id * itemImageWidth % itemSpriteSheet.Width)
-    (id * itemImageWidth / itemSpriteSheet.Width * itemImageHeight)
+    (int spriteIndex * itemImageWidth % spriteSheet.Width)
+    (int spriteIndex * itemImageWidth / spriteSheet.Width * itemImageHeight)
     itemImageWidth
     itemImageHeight
-    itemSpriteSheet
+    spriteSheet
 
 
 [<EntryPoint>]
@@ -318,7 +342,7 @@ let main args =
   let config =
     let json =
       try File.ReadAllText configPath
-      with _ -> eprintfn "Error reading config file."; reraise ()
+      with _ -> eprintfn "Error reading config file"; reraise ()
 
     match Decode.fromString Decode.config json with
     | Ok config -> config
@@ -342,61 +366,83 @@ let main args =
           then graphicsManager :> obj
           else null
     }
-    new Content.ContentManager (dummyServiceProvider, stardewValleyRoot)
+    new Content.ContentManager (dummyServiceProvider, Path.Combine (stardewValleyRoot, "Content"))
 
-  let tryLoad name path =
-    try content.Load path
-    with _ -> eprintfn $"Error loading {name}."; reraise ()
+  let mutable lastId = unknownIdStart
+  let itemIds = Dictionary ()
 
-  let inline tryLoadData name path =
-    (tryLoad $"{name} data" path: Dictionary<int,_>)
-    |> Seq.map (fun (KeyValue (k, v)) -> nat k * 1u<_>, v)
+  let assignId (strId: string) =
+    match itemIds.TryGetValue strId with
+    | true, itemId -> itemId
+    | false, _ ->
+      let itemId = tryParseNat strId |> Option.defaultWith (fun () ->
+        let itemId = lastId
+        lastId <- lastId + 1u
+        itemId)
+      let itemId = itemId * 1u<_>
+      itemIds.Add (strId, itemId)
+      itemId
 
-  let cropData =
-    tryLoadData "crop" cropDataPath
-    |> Seq.filter (fst >> config.SkipCrops.Contains >> not)
+  let tryLoad name (path: string) =
+    try content.Load (path.Replace ('\\', Path.DirectorySeparatorChar))
+    with _ -> eprintfn $"Error loading {name}"; reraise ()
+
+  let cropData: Dictionary<string, StardewValley.GameData.Crops.CropData> = tryLoad "crop data" cropDataPath
+  let itemData: Dictionary<string, StardewValley.GameData.Objects.ObjectData> = tryLoad "item data" itemDataPath
+
+  let crops =
+    cropData
+    |> Seq.choose (fun (KeyValue(seed, crop)) ->
+      if tryParseNat seed |> Option.exists (fun seed -> config.SkipCrops.Contains (seed * 1u<_>))
+      then None
+      else Some (assignId seed * 1u<_>, (seed, crop)))
+    |> Seq.map (fun (seed, (strId, data)) -> processCrop config.FarmCropOverrides config.ForageCropData assignId strId seed data)
     |> Array.ofSeq
-
-  let itemData = tryLoadData "item" itemDataPath |> Table.ofSeq
-
-  let inline tryLoadSpriteSheet name path: Texture2D = tryLoad $"{name} spritesheet" path
-
-  let cropSpriteSheet = tryLoadSpriteSheet "crop" cropSpriteSheetPath
-  let itemSpriteSheet = tryLoadSpriteSheet "item" itemSpriteSheetPath
-
-  let crops = cropData |> Array.map (fun (seed, data) ->
-    parseCrop config.FarmCropOverrides config.ForageCropData seed data)
   crops |> Array.sortInPlaceBy (snd >> Crop.seed)
 
+  config.Products.Values |> Seq.collect (Seq.map _.Item) |> Seq.iter (string >> assignId >> ignore)
+
+  let itemData =
+    itemData
+    |> Seq.map (fun (KeyValue(strId, item)) -> strId, item)
+    |> Table.ofSeq
+
   let items =
-    [|
-      config.Products.Values |> Seq.collect (Seq.map _.Item)
-      crops |> Seq.collect (snd >> Crop.items)
-      crops |> Seq.map (snd >> Crop.seedItem)
-    |]
-    |> Seq.concat
-    |> Seq.distinct
-    |> Seq.map (parseItem config.ItemOverrides itemData)
+    itemIds
+    |> Seq.map (fun (KeyValue(strId, itemId)) -> processItem config.ItemOverrides itemData strId itemId)
     |> Array.ofSeq
-  items |> Array.sortInPlaceBy _.Id
+  items |> Array.sortInPlaceBy (snd >> _.Id)
 
   printfn "Successfully parsed all crops and items. Press enter to output the data file and images."
   |> System.Console.ReadLine
   |> ignore
 
+  let itemSpriteSheets =
+    items
+    |> Seq.map (fst >> snd)
+    |> Seq.distinct
+    |> Seq.map (fun texture ->
+      if texture = null
+      then itemSpriteSheetPath, tryLoad $"item spritesheet" itemSpriteSheetPath
+      else texture, tryLoad $"item spritesheet: {texture}" texture)
+    |> Table.ofSeq
+
   try
     Directory.CreateDirectory config.ItemImageOutputPath |> ignore
-    items |> Array.iter (saveItemImage graphics config.ItemImageOutputPath itemSpriteSheet)
-  with _ -> eprintfn "Error writing the item images."; reraise ()
+    items |> Array.iter (fun (spriteData, item) ->
+      saveItemImage graphics config.ItemImageOutputPath itemSpriteSheets spriteData item)
+  with _ -> eprintfn "Error writing the item images"; reraise ()
+
+  let cropSpriteSheet = tryLoad "crop spritesheet" cropSpriteSheetPath
 
   try
     Directory.CreateDirectory config.CropImageOutputPath |> ignore
     crops |> Array.iter (fun (spriteSheetRow, crop) ->
       saveStageImages graphics config.CropImageOutputPath cropSpriteSheet spriteSheetRow crop)
-  with _ -> eprintfn "Error writing the crop images."; reraise ()
+  with _ -> eprintfn "Error writing the crop images"; reraise ()
 
   let data = {
-    Items = items
+    Items = items |> Array.map snd
     Products = config.Products
     FarmCrops = crops |> Array.choose (function | (_, FarmCrop crop) -> Some crop | _ -> None)
     ForageCrops = crops |> Array.choose (function | (_, ForageCrop crop) -> Some crop | _ -> None)
@@ -405,6 +451,6 @@ let main args =
   let dataStr = data |> Encode.extractedData |> Encode.toString 2
 
   try File.WriteAllText (config.DataOutputPath, dataStr)
-  with _ -> eprintfn "Error writing the data file."; reraise ()
+  with _ -> eprintfn "Error writing the data file"; reraise ()
 
   0
